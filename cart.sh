ee059943e01d6e69340711de03d5ad334f951234
harish <harish3212000@gmail.com>
2022-11-27 15:48:46 -0500

    cart service

--- cart.sh
+++ cart.sh
@@ -1,53 +1,54 @@
+ COMPONENT=Cart
  source common.sh
 
  PRINT "INSTALL NODEJS REPOS"
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
  STAT $?
 
  PRINT "INSTALL NODEJS"
- yum install nodejs -y
+ yum install nodejs -y &>>$LOG
  STAT $?
 
  PRINT "ADD APPLICATION USER"
- useradd roboshop
+ useradd roboshop &>>$LOG
 STAT $?
 
 PRINT "DOWNLOAD APP CONTENT"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>$LOG
 STAT $?
 
 PRINT "REMOVE PREVIOUS VERSION OF APP"
-cd /home/roboshop
-rm -rf cart
+cd /home/roboshop &>>$LOG
+rm -rf cart &>>$LOG
 STAT $?
 
 PRINT "EXTRACTING APP CONTENT"
-unzip -o /tmp/cart.zip
+unzip -o /tmp/cart.zip &>>$LOG
 STAT $?
 
 mv cart-main cart
 cd cart
 
 PRINT "INSTALL NODEJS DEPENDENCIES"
-npm install
+npm install &>>$LOG
 STAT $?
 
 PRINT "CONFIGURE ENDPOINTS FOR SYSTEMD CONFIGERATION"
-sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/cart/systemd.service
+sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/cart/systemd.service &>>$LOG
 STAT $?
 
 PRINT "SETUP SYSTEMD SERVICES"
-mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>$LOG
 STAT $?
 
 PRINT "RELOAD SYSTEMD"
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG
 STAT $?
 
 PRINT"RESTART CART"
-systemctl restart cart
+systemctl restart cart &>>$LOG
 STAT $?
 
 PRINT"ENABLE CART SERVICE"
-systemctl enable cart
+systemctl enable cart &>>$LOG
 STAT $?
--- common.sh
+++ common.sh
@@ -8,3 +8,6 @@ STAT() {
 PRINT() {
   echo -e "\e[33m$1\e[0m"
 }
+
+LOG= /tmp/$COMPONENT.log
+rm-rf $LOG
