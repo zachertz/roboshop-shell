7080140fb230cc5e4a793749ff849e7d31efd32b
harish <harish3212000@gmail.com>
2022-11-27 10:07:06 -0500

    my sql script

--- mysql.sh
+++ mysql.sh
@@ -1,35 +1,23 @@
+
+STAT() {
+  if [ $1 -eq 0 ]; then
+          echo SUCCESS
+        else
+            echo FAILURE
+          fi
+          }
 echo -e "\e[32mDowloading MySQL repos file\e[om"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-    echo FAILURE
-  fi
+STAT $?
   echo Disable MySQL 8 version repo
 dnf module disable mysql -y
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-    echo FAILURE
-  fi
+STAT $?
   echo install MySQL
 yum install mysql-community-server -y
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-    echo FAILURE
-  fi
+STAT $?
   echo enable MySQL service
 systemctl enable mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-    echo FAILURE
-  fi
+STAT $?
   echo  Start MySQL service
 systemctl restart mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-    echo FAILURE
-  fi
+STAT $?
